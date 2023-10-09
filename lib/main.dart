453fab72e19316014e42f08574db0961a4dc4786
Ferdiansyahumdp <mf3rdiansyah04@mhs.mdp.ac.id>
2023-10-09 10:50:20 +0700

    Ganti Judul
    mengganti judul sesuai issue no 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aplikasi Masakan indonesia 7',
       theme: ThemeData(
         // This is the theme of your application.
         //
