702638b343cdf0175de252105a72c02f4e3ffd5c
DerylAndeya <derylandeya@gmail.com>
2023-12-17 18:35:25 +0700

    updated

--- lib/data/masakan_data.dart
+++ lib/data/masakan_data.dart
@@ -6,7 +6,7 @@ var MasakanList = [
     origin: 'Minangkabau, Sumatera Barat',
     description:
     'Rendang, hidangan yang berasal dari Minangkabau, Sumatra Barat, Indonesia, adalah sebuah masterpiece kuliner yang memikat dengan kekayaan rasa dan aroma yang mendalam. Daging sapi yang dipotong kecil-kecil, sering kali menggunakan potongan daging yang berlemak dan berserat, dimasak dalam santan kelapa yang pekat dan dipadu dengan sejumlah besar rempah-rempah. Proses memasaknya yang lambat dan intens menghasilkan daging yang empuk, berwarna gelap, dan bumbu yang meresap hingga ke setiap seratnya. Bumbu rendang melibatkan campuran rempah-rempah seperti serai, daun salam, daun jeruk, lengkuas, bawang merah, bawang putih, cabe merah, ketumbar, jintan, dan kencur. Hasilnya adalah kuah yang kental, gurih, dan penuh dengan cita rasa yang kompleks. Rendang sering kali disajikan dengan nasi putih, dan kelezatannya meningkat seiring waktu karena bumbu meresap lebih dalam ke dalam daging. Hidangan ini bukan hanya sebuah sajian, melainkan perwujudan seni kuliner Indonesia yang kaya dan mendalam, menciptakan pengalaman makan yang tak terlupakan.',
-    imageAsset: 'images/',
+    imageAsset: 'images/rendang.jpg',
     imageUrls: [
       'https://th.bing.com/th/id/OIP.qNWDKaTrWQ4Fur2D8cNgvwHaFS?pid=ImgDet&w=196&h=139&c=7&dpr=1,5',
       'https://img.theculturetrip.com/768x/wp-content/uploads/2018/04/shutterstock_554112613.jpg',
@@ -20,7 +20,7 @@ var MasakanList = [
     origin: 'Palembang, Sumatera Selatan',
     description:
     'Pempek, hidangan khas Palembang, Sumatra Selatan, Indonesia, adalah kelezatan unik yang menggabungkan ikan, tepung sagu, dan rempah-rempah dalam satu sajian istimewa. Pempek biasanya terdiri dari adonan yang terbuat dari ikan tenggiri yang dihaluskan, dicampur dengan tepung sagu, dan bumbu-bumbu seperti garam dan lada. Adonan kemudian dibentuk menjadi berbagai bentuk, seperti bulat (kapal selam), pipih (lenjer), atau bulat pipih (adaan). Kemudian, pempek direbus atau dikukus sebelum dipanggang atau digoreng untuk memberikan tekstur yang kenyal dan lembut. Pempek selanjutnya disajikan dengan kuah cuko, saus yang terbuat dari campuran gula aren, cuka, air, bawang putih, dan cabe rawit. Kuah cuko ini memberikan sentuhan asam, manis, dan pedas yang menyatu dengan sempurna, menambahkan dimensi rasa yang kaya pada hidangan. Pempek sering disajikan dengan irisan telur rebus dan tauge sebagai pelengkap. Kelezatan Pempek tidak hanya terletak pada cita rasa yang unik, tetapi juga pada pengalaman kuliner yang menyenangkan, menggabungkan kekenyalan ikan, keharuman rempah, dan sensasi rasa cuko yang memukau.',
-    imageAsset: 'images/',
+    imageAsset: 'images/pempek.jpg',
     imageUrls: [
       'https://media.istockphoto.com/id/1470160209/id/foto/pempek.jpg?b=1&s=612x612&w=0&k=20&c=-MSYffXGEYBw4oM3DY3a6PDjFrE_D_VvHEFoMC98sLY=',
       'https://media.istockphoto.com/id/1361196001/id/foto/makanan-tradisional.jpg?b=1&s=612x612&w=0&k=20&c=y915kUZ59WlnHG8MKG-dlubrK5eCaZIMDSM9BA2PJ2E=',
@@ -34,7 +34,7 @@ var MasakanList = [
     origin: 'Padang, Sumatera Barat',
     description:
     'Sate Padang, hidangan yang berasal dari Padang, Sumatra Barat, Indonesia, menggoda selera dengan cita rasa yang kaya, pedas, dan lezat. Hidangan ini terdiri dari potongan-potongan daging, biasanya daging sapi, yang ditusuk kecil-kecil dan dipanggang hingga matang di atas bara api. Namun, keunikan Sate Padang terletak pada kuah kacang khasnya yang sangat lezat. Kuah kacang ini disiapkan dengan mencampurkan bumbu rempah seperti ketumbar, kemiri, cabe merah, bawang merah, dan bawang putih yang dihaluskan, lalu direbus bersama dengan santan kelapa hingga mengental. Selain itu, sate ini juga sering dilengkapi dengan potongan nasi ketan atau lontong, serta irisan bawang merah dan daun seledri sebagai hiasan. Rasa pedas dan gurih kuah kacang Sate Padang menciptakan paduan cita rasa yang menggugah selera, menjadikannya sebagai salah satu hidangan sate yang paling dicari di Indonesia..',
-    imageAsset: 'images/',
+    imageAsset: 'images/satepadang.jpg',
     imageUrls: [
       'https://media.istockphoto.com/id/1337983615/photo/sate-padang.jpg?b=1&s=612x612&w=0&k=20&c=GM-Uwz65eruVuMCaJQG7HTfurhDoMPrrxqUg3UacfSc=',
       'https://media.istockphoto.com/id/1434531397/photo/sate-padang-and-rice-cake.jpg?b=1&s=612x612&w=0&k=20&c=5Ea8Itim2rIxsW2EBwDRQ-R2Cy-g8gw4_YXxVi9Cr_c=',
@@ -48,7 +48,7 @@ var MasakanList = [
     origin: 'Yogyakarta',
     description:
     'Gudeg, sebuah hidangan tradisional dari Yogyakarta, Indonesia, adalah paduan lezat antara nangka muda yang dimasak dalam santan, gula kelapa, dan berbagai rempah-rempah yang khas. Dengan warna coklat kekuningan yang menggoda, Gudeg menyajikan keharuman rempah yang meresap dan rasa manis lembut dari campuran santan dan gula kelapa. Potongan nangka muda yang lunak dan empuk, disertai dengan potongan daging, dimasak dalam kelezatan bumbu-bumbu seperti daun salam, serai, ketumbar, dan kelapa parut. Gudeg umumnya disajikan dengan nasi putih, telur rebus, dan ayam kampung atau abon sebagai pelengkapnya. Keunikan Gudeg terletak pada penyajiannya yang memberikan pengalaman rasa yang seimbang antara manis dan gurih, menjadikannya sebagai salah satu hidangan khas Indonesia yang tak terlupakan.',
-    imageAsset: 'images/',
+    imageAsset: 'images/gudeg.jpg',
     imageUrls: [
       'https://th.bing.com/th?id=OIP.2HfedVk5gpYKca6yNYV3lAHaFj&w=288&h=216&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2',
       'https://www.indonesia.travel/content/dam/indtravelrevamp/en/destinations/revisi-2020/thumbnailgudeg.jpg',
@@ -62,7 +62,7 @@ var MasakanList = [
     origin: 'Surabaya, Jawa Timur',
     description:
     'Rawon adalah hidangan khas Jawa Timur, Indonesia, yang memikat dengan warna hitam pekat dan cita rasa yang khas. Hidangan ini terkenal karena kuahnya yang gurih dan berwarna hitam, yang diperoleh dari penggunaan kluwek, sejenis buah yang memberikan warna dan rasa khas. Potongan daging sapi, sering kali berupa daging empuk seperti tengkleng atau empal, dimasak dalam kuah rawon yang diperkaya dengan rempah-rempah seperti ketumbar, kunyit, lengkuas, dan daun salam. Selain daging, hidangan ini juga dapat mengandung telur asin dan tauge sebagai pelengkap. Rawon biasanya disajikan dengan nasi putih dan dilengkapi dengan sambal terasi, irisan jeruk limau, dan emping sebagai tambahan tekstur. Kelezatan rawon tidak hanya terletak pada cita rasa yang kaya dan unik, tetapi juga pada harmoni antara rempah-rempah dan bumbu-bumbu yang digunakan, menciptakan sebuah pengalaman kuliner yang mendalam dan otentik dari Indonesia..',
-    imageAsset: 'images/',
+    imageAsset: 'images/rawon.jpg',
     imageUrls: [
       'https://thumbs.dreamstime.com/b/rawon-typical-indonesian-food-294453386.jpg',
       'https://thumbs.dreamstime.com/b/rawon-soup-tasty-delicious-rawon-soup-tasty-delicious-175264745.jpg',
@@ -76,7 +76,7 @@ var MasakanList = [
     origin: 'Banjarmasin, Kalimantan Selatan',
     description:
     'Soto Banjar, hidangan khas dari Banjarmasin, Kalimantan Selatan, Indonesia, memikat lidah dengan keunikan rasa dan keharuman bumbunya. Dihidangkan dalam mangkuk berisi kuah yang kaya dan hangat, soto ini menggabungkan potongan daging sapi yang empuk dengan irisan usus atau tetelan sapi, menciptakan paduan rasa yang lezat. Kuahnya, yang diperoleh dari kaldu daging sapi dan tulang, meresap dengan rempah-rempah seperti ketumbar, kunyit, lada, serai, daun salam, dan daun jeruk, memberikan aroma harum yang menggoda dan rasa yang khas. Soto Banjar disajikan dengan mie kuning atau bihun, serta diperkaya dengan tauge, kentang rebus, telur rebus, bawang goreng, dan seledri. Tambahan sambal terasi atau sambal kecap memberikan pilihan rasa yang beragam. Dengan cita rasa yang autentik dan komposisi bumbu yang unik, Soto Banjar menjadi bagian tak terpisahkan dari warisan kuliner Indonesia, menciptakan pengalaman kuliner yang memuaskan dan menggoda selera..',
-    imageAsset: 'images/',
+    imageAsset: 'images/sotobanjar.jpg',
     imageUrls: [
       'https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/indizone/2022/11/28/M7skyok/resep-soto-banjar-asli-khas-kalimantan-selatan-simple-banget80.jpg',
       'https://www.poultryindonesia.com/wp-content/uploads/2022/07/Soto-Banjar-Sumber-Wikipedia.jpeg',
@@ -90,7 +90,7 @@ var MasakanList = [
     origin: 'Makassar, Sulawesi Selatan',
     description:
     'Coto Makassar adalah hidangan khas yang berasal dari daerah Makassar, Sulawesi Selatan, Indonesia. Hidangan ini menggoda selera dengan kuah kentalnya yang melibatkan campuran daging sapi, jeroan, dan paru sapi yang dimasak hingga empuk. Kelezatan Coto Makassar tidak hanya terletak pada kekayaan rasa daging, tetapi juga pada harmoni rempah-rempah yang memenuhi kuahnya. Bumbu-bumbu tradisional seperti ketumbar, kunyit, lada, jahe, bawang merah, bawang putih, dan serai memberikan cita rasa khas yang memikat. Hidangan ini biasanya disajikan dengan ketupat atau burasa, serta dilengkapi sambal pedas dan irisan jeruk limau untuk menyempurnakan pengalaman kuliner. Coto Makassar bukan hanya sekadar makanan, tetapi juga sebuah warisan kulinernya yang membanggakan, menjadi pilihan populer untuk dinikmati sebagai hidangan sarapan atau makan malam di berbagai kesempatan.',
-    imageAsset: 'images/',
+    imageAsset: 'images/cotomakassar.jpg',
     imageUrls: [
       'https://asset.kompas.com/crops/ljY1r3f3HJaNEuLip3iARQOaK-E=/0x1226:3000x3226/750x500/data/photo/2021/06/11/60c2acf88c6f0.jpg'
       'https://cdn.shopify.com/s/files/1/0428/0965/5452/files/coto_1_pinterest_piknikdong.jpg?v=1669740131'
@@ -104,7 +104,7 @@ var MasakanList = [
     origin: 'Manado, Sulawesi Utara',
     description:
     'Bubur Manado, hidangan khas dari Manado, Sulawesi Utara, Indonesia, menghadirkan pengalaman kuliner yang unik dan menggugah selera. Bubur ini terbuat dari beras yang dimasak hingga menjadi bubur kental dan lembut, lalu diberi sentuhan khas Manado melalui berbagai bumbu dan tambahan yang membuatnya istimewa. Salah satu ciri khasnya adalah pemakaian ikan cakalang yang diasap atau digoreng sebagai pelengkap, memberikan cita rasa gurih dan asap yang khas. Selain itu, Bubur Manado diperkaya dengan bumbu-bumbu rempah seperti serai, daun jeruk, daun salam, jahe, bawang merah, bawang putih, dan cabe rawit yang memberikan sentuhan pedas yang membangkitkan selera. Bubur ini juga biasanya disajikan dengan irisan tomat, daun seledri, dan perasan jeruk nipis untuk memberikan kesegaran. Kombinasi antara tekstur lembut dari bubur dan keharuman serta kepedasan dari bumbu-bumbu menjadikan Bubur Manado sebagai hidangan yang menggoda, memikat, dan memperkaya ragam kuliner Indonesia.',
-    imageAsset: 'images/plaosan.jpeg',
+    imageAsset: 'images/buburmanado.jpeg',
     imageUrls: [
       'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/07/28050205/ini-cara-membuat-bubur-manado-yang-lezat-dan-kaya-nutrisi.jpg.webp'
       'https://asset.kompas.com/crops/OW8rRJecUkSu5PK-Zg7cEHPejgk=/2x0:700x465/750x500/data/photo/2022/05/31/6295c8b093cfa.jpg'
@@ -118,7 +118,7 @@ var MasakanList = [
     origin: 'Nusa Tenggara Barat',
     description:
     'Ayam Taliwang, hidangan ikonik dari pulau Lombok, memukau lidah dengan harmoni cita rasa yang pedas dan gurih. Daging ayam kampung dipersiapkan secara khusus dengan bumbu rempah yang melibatkan kemiri, bawang merah, bawang putih, daun jeruk, serai, dan cabai rawit. Keunikan Ayam Taliwang terletak pada cara pengolahan ayam ini, yang dapat dipanggang atau dibakar, memberikan lapisan luar renyah dan aroma panggang yang menggoda. Rempah-rempah yang meresap dalam daging menciptakan cita rasa yang mendalam, sedangkan kepedasan cabai rawit memberikan sentuhan pedas yang menggigit. Hidangan ini biasanya disajikan dengan plecing kangkung, sayuran kangkung yang diberi sambal terasi, menciptakan kontrast yang menyegarkan. Ayam Taliwang bukan hanya makanan, melainkan perwujudan seni kuliner yang memadukan kekayaan rempah-rempah Indonesia dengan keahlian memasak lokal, menciptakan sebuah pengalaman gastronomi yang tak terlupakan.',
-    imageAsset: 'images/',
+    imageAsset: 'images/ayamtaliwang.jpg',
     imageUrls: [
       'https://asset.kompas.com/crops/-oAqlkfrKHaqebhytdtKz-JV6ls=/4x0:1000x664/750x500/data/photo/2020/12/22/5fe16c7655542.jpg'
       'https://asset.kompas.com/crops/-oAqlkfrKHaqebhytdtKz-JV6ls=/4x0:1000x664/750x500/data/photo/2020/12/22/5fe16c7655542.jpg'
@@ -132,7 +132,7 @@ var MasakanList = [
     origin: 'Papua',
     description:
     'Papeda, hidangan khas Maluku dan Papua, Indonesia, adalah perpaduan unik dari sagu dan air yang menghasilkan hidangan pati yang lekat dan kenyal. Proses persiapan Papeda melibatkan pencampuran tepung sagu dengan air dan diaduk hingga membentuk adonan yang kental. Hidangan ini sering disajikan dengan kuah ikan, cakalang, atau tongkol yang kaya rempah, yang memberikan cita rasa gurih dan lezat. Papeda biasanya dimakan bersama lauk pauk khas Maluku dan Papua, seperti ikan kuah kuning, ikan tongkol, atau ikan bakar. Keunikan Papeda terletak pada tekstur kenyal dan lekatnya, yang membutuhkan keterampilan khusus dalam mengonsumsinya. Biasanya dimakan dengan cara dicabik kecil menggunakan dua jari, kemudian dicelupkan ke dalam kuah ikan sebelum dimakan. Papeda tidak hanya makanan pokok, tetapi juga warisan kultural yang memperkaya keanekaragaman kuliner Indonesia. Hidangan ini menciptakan pengalaman menyantap yang berbeda, menggabungkan sederhana tapi kaya rasa, serta memperlihatkan kekayaan bahan-bahan alam lokal.',
-    imageAsset: 'images/',
+    imageAsset: 'images/papeda.jpg',
     imageUrls: [
       'https://bangunpapua.com/storage/2022/07/Ilustrasi-Papeda.jpg'
       'https://dispar.malukuprov.go.id/wp-content/uploads/2021/01/PAPEDA-IKAN-KUAH-KUNING-scaled.jpg'
